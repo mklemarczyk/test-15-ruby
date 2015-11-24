1e3a66145899d5fcfb0dc6aaa75efcea2ddb94b2
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-11-24 11:59:32 +0100

    Matrix geters were created

--- example/math.rb
+++ example/math.rb
@@ -3,9 +3,9 @@ class Vector
 	@y
 	@z
 	def initialize(x, y, z)
-		@x = x
-		@y = y
-		@z = z
+		@x = Float(x)
+		@y = Float(y)
+		@z = Float(z)
 	end
 	def x()
 		@x
@@ -117,24 +117,72 @@ class Matrix
 	@m43
 	@m44
 	def initialize(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44)
-		@m11 = m11
-		@m12 = m12
-		@m13 = m13
-		@m14 = m14
+		@m11 = Float(m11)
+		@m12 = Float(m12)
+		@m13 = Float(m13)
+		@m14 = Float(m14)
 
-		@m21 = m21
-		@m22 = m22
-		@m23 = m23
-		@m24 = m24
+		@m21 = Float(m21)
+		@m22 = Float(m22)
+		@m23 = Float(m23)
+		@m24 = Float(m24)
 
-		@m31 = m31
-		@m32 = m32
-		@m33 = m33
-		@m34 = m34
+		@m31 = Float(m31)
+		@m32 = Float(m32)
+		@m33 = Float(m33)
+		@m34 = Float(m34)
 
-		@m41 = m41
-		@m42 = m42
-		@m43 = m43
-		@m44 = m44
+		@m41 = Float(m41)
+		@m42 = Float(m42)
+		@m43 = Float(m43)
+		@m44 = Float(m44)
+	end
+	def m11()
+		@m11
+	end
+	def m12()
+		@m12
+	end
+	def m13()
+		@m13
+	end
+	def m14()
+		@m14
+	end
+	def m21()
+		@m21
+	end
+	def m22()
+		@m22
+	end
+	def m23()
+		@m23
+	end
+	def m24()
+		@m24
+	end
+	def m31()
+		@m31
+	end
+	def m32()
+		@m32
+	end
+	def m33()
+		@m33
+	end
+	def m34()
+		@m34
+	end
+	def m41()
+		@m41
+	end
+	def m42()
+		@m42
+	end
+	def m43()
+		@m43
+	end
+	def m44()
+		@m44
 	end
 end
