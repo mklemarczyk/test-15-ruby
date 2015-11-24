b09c38965efa40c8cdea8d56138d8d364d7aa3ab
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-11-24 11:55:25 +0100

    New tests were implemented to cover next few methods of Vector.

--- example/testing.rb
+++ example/testing.rb
@@ -90,3 +90,127 @@ RSpec.describe Vector do
 		end
 	end
 end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector multiply operation works" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			v = v1.multiply(v2)
+			
+			expect(v.x).to eq 16
+			expect(v.y).to eq 33
+			expect(v.z).to eq 52
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,24,20)" do
+		it "checks if vector divide operation works" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,24,20)
+			v = v1.divide(v2)
+
+			expect(v.x).to eq 0.25
+			expect(v.y).to eq 0.125
+			expect(v.z).to eq 0.2
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector min computes correctly" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			v = v1.min(v2)
+			
+			expect(v.x).to eq v1.x
+			expect(v.y).to eq v1.y
+			expect(v.z).to eq v1.z
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector max computes correctly" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			v = v1.max(v2)
+			
+			expect(v.x).to eq v2.x
+			expect(v.y).to eq v2.y
+			expect(v.z).to eq v2.z
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector cross computes correctly" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			v = v1.cross(v2)
+			
+			expect(v.x).to eq -5
+			expect(v.y).to eq 6
+			expect(v.z).to eq -2
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector reflect computes correctly" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			v = v1.reflect(v2)
+
+			expect(v.x).to eq -1614
+			expect(v.y).to eq -2219
+			expect(v.z).to eq -2622
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(17,18,19) m1=(1,2,3,4, 5,6,7,8, 9,10,11,12, 13,14,15,16)" do
+		it "checks if vector transform computes correctly" do
+			v1 = Vector.new(17,18,19)
+			m1 = Matrix.new(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+			v = v1.transform(m1)
+			
+			expect(v.x).to eq 291
+			expect(v.y).to eq 346
+			expect(v.z).to eq 401
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector dot product computes correctly" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			d = v1.dot(v2)
+			
+			expect(d).to eq 101
+		end
+	end
+end
+
+RSpec.describe Vector do
+	context "with v1=(2,3,4) v2=(8,11,13)" do
+		it "checks if vector distance computes correctly" do
+			v1 = Vector.new(2,3,4)
+			v2 = Vector.new(8,11,13)
+			d = v1.distance(v2)
+
+			expect(d).to eq 13.45362404707371031716308546217
+		end
+	end
+end
+
+
