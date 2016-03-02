5383cb82795a0409f283986311007592c3a91df7
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-03-02 14:54:03 +0100

    Wczesna implementacja zadania 1 z projektu 1.

---
+++ java/Proj1Z1/src/ug/lab/project1/MyStack.java
@@ -0,0 +1,30 @@
+package ug.lab.project1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStack {
+	
+	private List<Integer> data = new ArrayList<Integer>();
+	
+	public void MyPush(int a){
+		data.add(a);
+	}
+	
+	public void MyPop(){
+		data.remove(data.size() -1);
+	}
+	
+	public boolean IsNull(){
+		return data.size() == 0;
+	}
+	
+	public int MyTop(){
+		return data.get(data.size() -1);
+	}
+
+	public List<Integer> getData() {
+		return data;
+	}
+
+}
