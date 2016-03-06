d9b8833e9ee181fc73dd4644728d3e2cc375be22
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-03-06 23:24:40 +0100

    [Java] Utworzenie interfejsu do zadania 2 z Lab 3.

---
+++ java/Proj1Z2/src/ug/lab/project1/IPsikus.java
@@ -0,0 +1,15 @@
+package ug.lab.project1;
+
+public interface IPsikus {
+
+	Integer CyfroKrad(Integer liczba);
+	
+	Integer HultajChochla(Integer liczba) throws NieudanyPsikusException;
+	
+	Integer Nieksztaltek (Integer liczba);
+	
+	Integer Heheszki(Integer liczba);
+	
+	boolean Titit(Integer liczba, Integer liczba_dziel);
+
+}
---
+++ java/Proj1Z2/src/ug/lab/project1/NieudanyPsikusException.java
@@ -0,0 +1,10 @@
+package ug.lab.project1;
+
+public class NieudanyPsikusException extends Exception {
+
+	/**
+	 * Eclipse want it
+	 */
+	private static final long serialVersionUID = 6179085602391983148L;
+
+}
