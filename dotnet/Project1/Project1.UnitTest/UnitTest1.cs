4a0ecb4269515600c4b25242c96b7ea33948ea89
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-04-04 10:36:28 +0200

    Blank dot net project1 was added.

---
+++ dotnet/Project1/Project1.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project1.UnitTest {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1( ) {
+		}
+	}
+}
