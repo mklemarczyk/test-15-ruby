82e7b71d56e7a662e082b7da714da803acb550a9
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-05-18 10:32:01 +0200

    Initial web project

---
+++ dotnet/Project3/MyProject/ShopWebApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ShopWebApp.Startup))]
+
+namespace ShopWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
