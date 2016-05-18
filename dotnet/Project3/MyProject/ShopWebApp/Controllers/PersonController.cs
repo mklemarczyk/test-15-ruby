1e847ca2cc51a0e569f0b838ca931ec094379b83
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-05-18 11:36:11 +0200

    Implemented load tests.

--- dotnet/Project3/MyProject/ShopWebApp/Controllers/PersonController.cs
+++ dotnet/Project3/MyProject/ShopWebApp/Controllers/PersonController.cs
@@ -10,7 +10,64 @@ namespace ShopWebApp.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Title = "Person Index";
+
+            var pp = new Models.AdventureWorksDataContext();
+
+            var dat = from item in pp.Persons
+                      select item;
+
+            ViewBag.Persons = dat.Take(100);
+
+            return View();
+        }
+
+        public ActionResult SortIndex1()
+        {
+            ViewBag.Title = "Sorted Person Index";
+
+            var pp = new Models.AdventureWorksDataContext();
+
+            var dat = (from item in pp.Persons
+                       select item).ToArray();
+
+            var ordered = (from item in dat
+                           orderby item.LastName, item.FirstName
+                           select item).ToArray();
+
+            ViewBag.Persons = ordered.Take(100);
+
+            return View();
+        }
+
+        public ActionResult SortIndex2()
+        {
+            ViewBag.Title = "Sorted Person Index";
+
+            var pp = new Models.AdventureWorksDataContext();
+
+            var dat = (from item in pp.Persons
+                       orderby item.LastName, item.FirstName
+                       select item).ToArray();
+
+            var ordered = (from item in dat
+                           select item).ToArray();
+
+            ViewBag.Persons = ordered.Take(100);
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
             ViewBag.Title = "Home Page";
+
+            var pp = new Models.AdventureWorksDataContext();
+
+            var dat = from item in pp.Persons
+                      select item;
+
+            ViewBag.Persons = dat;
 
             return View();
         }
